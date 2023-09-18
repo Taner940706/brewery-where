ca8fb0022567691ed3810bf9121764c8dea0cb2d
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-18 16:02:18 +0300

    Delete django_project directory

--- django_project/brewerywhere/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django_project.brewerywhere.views import Home, Single
-
-urlpatterns = (
-    path('', Home.as_view(), name='home'),
-    path('<id:string>/', Single.as_view(), name='single'),
-)
