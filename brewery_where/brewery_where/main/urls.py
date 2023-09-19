51dffec0af96197002b6c3e1f46bf06f57c25681
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-19 20:13:36 +0300

    config static, add style.css, create base.html

--- brewery_where/brewery_where/main/urls.py
+++ brewery_where/brewery_where/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from brewery_where.main.views import SingleView, HomeView
+
 urlpatterns = [
     path('<int:id>/', SingleView.as_view(), name='single'),
     path('', HomeView.as_view(), name="home"),
